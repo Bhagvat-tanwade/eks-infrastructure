19ec841582b1fca834a53ce35897416a9499b91d
Bhagvat-tanwade <tanwadebhagvat013@gmail.com>
2026-09-08 20:05:57 +0530

    Create S3 bucket with versioning and access settings
    Define an S3 bucket with versioning and public access controls.

---
+++ s3.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket = "cdec-studentapp-frontend-005008920274"
+
+  tags = {
+    Name        = "cdec-studentapp-frontend"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
